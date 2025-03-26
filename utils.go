8ad1042cb555b32409d431e74ed1812239045d38
暮晨 <leisurelicht@gmail.com>
2025-03-26 11:23:05 +0800

    safely delete unused variables

--- utils.go
+++ utils.go
@@ -58,7 +58,6 @@ func StructSlice2MapSlice(obj any, tag string) []map[string]any {
 	t := reflect.TypeOf(obj)
 	v := reflect.ValueOf(obj)
 	if t.Kind() == reflect.Ptr {
-		t = t.Elem()
 		v = v.Elem()
 	}
 
